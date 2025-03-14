876b2e1f66fa4e9a5a01800a38cec84bf96df85d
DongHyeon Ko <hanser0204@naver.com>
2025-03-14 09:19:27 +0900

    Test

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by kosora on 3/5/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
