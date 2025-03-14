29de710169b0b6e194a64c54929ef4b3edc015d6
DongHyeon Ko <hanser0204@naver.com>
2025-03-14 09:27:49 +0900

    main.cpp 수정

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  Test
@@ -10,6 +11,6 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!\n";
+    cout << "Hello, World222!\n";
     return 0;
 }
